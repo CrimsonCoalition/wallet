6d0d84d0424881df63de47efa48f6cc2045d41a0
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-12-25 14:33:31 +0300

    Update keyboards.py

--- wallet/keyboards.py
+++ wallet/keyboards.py
@@ -1,4 +1,16 @@
-import aiogram
-from aiogram import types
+# импортируем из библиотеки aiogram все нужное.
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 
+# Кнопка пополнить средства
+inline_btn_1 = InlineKeyboardButton('Пополнить', callback_data='button1')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_1)
 
+# Кнопка вывести средства
+inline_btn_2 = InlineKeyboardButton('Вывести', callback_data='button2')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_2)
+
+# 
+inline_btn_3 = InlineKeyboardButton('Отправить', callback_data='button3')
+inline_kb1 = InlineKeyboardMarkup().add(inline_btn_3)
