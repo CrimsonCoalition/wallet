e0aa33e7111567f0630c85d96ab353299c8a5c7e
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-12-26 21:39:48 +0300

    Update donate.py

--- wallet/functions/donate.py
+++ wallet/functions/donate.py
@@ -1,1 +1,27 @@
-# Donate / Донат
+# Donate / Донат 
+
+#
+#
+#
+
+# Imports / Импорты
+import asyncio
+import logging
+
+from aiogram import Bot, types
+from aiogram.utils import executor
+from aiogram.dispatcher import Dispatcher
+from aiogram.types.message import ContentType
+
+from config import BOT_TOKEN, PAYMENTS_PROVIDER_TOKEN, TIME_MACHINE_IMAGE_URL
+
+
+logging.basicConfig(format=u'%(filename)+13s [ LINE:%(lineno)-4s] %(levelname)-8s [%(asctime)s] %(message)s',
+                    level=logging.INFO)
+
+loop = asyncio.get_event_loop() # Получаем текущий event loop. Тоесть запускаем работу нужных нам процессов.
+bot = Bot(BOT_TOKEN, parse_mode=types.ParseMode.MARKDOWN)
+dp = Dispatcher(bot, loop=loop)
+
+## Устанавливаем цену.
+PRICE = types.LabeledPrice(label='Поддержать разработку', amount=50)
